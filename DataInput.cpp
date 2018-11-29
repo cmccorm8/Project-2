3bc44733b82d545f4e835c71109e9688ae0592ca
James Schuchardt <43153708+JSchuch2@users.noreply.github.com>
2018-11-28 20:45:44 -0700

    Add files via upload

--- DataInput.cpp
+++ DataInput.cpp
@@ -9,11 +9,29 @@ using std::endl;
 using std::string;
 using std::to_string;
 
-string main()
+string gen_random()
+{
+	string final;
+	string temp;
+	string holder =
+		"0123456789"
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		"abcdefghijklmnopqrstuvwxyz";
+
+	for (int i = 0; i < 8; i++) 
+	{
+		temp = holder[rand() % (sizeof(holder))];
+		final.append(temp);
+	}
+	return final;
+
+}
+
+int main()
 {
 	string ParentID;
-
-	
-
-	return "Complete!";
+	ParentID = gen_random();
+	cout << ParentID << endl;
+	system("pause");
+	return 2;
 }
