749c1e38bcf37075f35b1626caaa406d3df02881
James Schuchardt <43153708+JSchuch2@users.noreply.github.com>
2018-11-28 20:11:33 -0700

    Add files via upload

---
+++ DataInput.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::to_string;
+
+string main()
+{
+	string ParentID;
+
+	
+
+	return "Complete!";
+}
---
+++ HashTable.h
@@ -0,0 +1,31 @@
+#ifndef _HASHTABLE_H_
+#define _HASHTABLE_H_
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+class HashTable
+{
+	struct hash
+	{
+	private:
+		string ID;
+		string Parent_ID;
+		string Raw_Event;
+		string RHASH;
+		string LHASH;
+		vector<string> RHISTH;
+		vector<string> LHISTH;
+	public:
+		hash *ptr;
+	};
+};
+
+HashTable
+#endif
