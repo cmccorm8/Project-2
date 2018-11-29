8f2cf7b4714ca843a5e07a8dfb4852d795479089
James Schuchardt <43153708+JSchuch2@users.noreply.github.com>
2018-11-29 05:17:33 -0700

    Add files via upload

--- DataInput.cpp
+++ DataInput.cpp
@@ -1,7 +1,13 @@
+//Chance McCormick, Carlton Wilcox, James Schuchardt
+//COSC 2030
+//Project 2 
+//11/28/2018
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <cmath>
+#include "BST.h"
 
 using std::cin;
 using std::cout;
@@ -9,40 +15,14 @@ using std::endl;
 using std::string;
 using std::to_string;
 
-string gen_random()
-{
-	string final;
-	string temp;
-	string holder =
-		"0123456789AaBbCcDdEeFfGgHhIjJiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
-
-	for (int i = 0; i < 8; i++) 
-	{
-		temp = holder[rand() % (sizeof(holder))];
-		final.append(temp);
-	}
-	return final;
-
-}
+BinSearchTree Tester;
 
 int main()
 {
-	string ParentID;
-	string Second;
-	string Second1;
-	string Second2;
-	string Second3;
-	ParentID = gen_random();
-	cout << ParentID << endl;
-	Second = gen_random();
-	cout << Second << endl;
-
-	Second1 = gen_random();
-	cout << Second1 << endl;
-	Second2 = gen_random();
-	cout << Second2 << endl;
-	Second3 = gen_random();
-	cout << Second3 << endl;
+	//Inputs the root node
+	Tester.createNode();
+	//Inputs the second node in the tree (this shoud end up being the left or right of the one above with null being produced on the other one.)
+	Tester.createNode();
 	system("pause");
 	return 2;
 }
